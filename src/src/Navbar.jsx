870256090b58268e79270c11bf50a60b4fbc9693
petes8796 <127255945+petes8796@users.noreply.github.com>
2025-11-08 12:13:52 +0530

    Add Navbar component with logo and login section

---
+++ src/src/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+export default function Navbar(){
+  return (
+    <div className="header">
+      <div className="logo">AVRAE</div>
+      <div className="member">MEMBER LOGIN</div>
+    </div>
+  );
+}
